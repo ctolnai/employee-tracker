2224be284bb7a47702043d3cf74a91105d80b2ad
Chris Tolnai <christolnai@gmail.com>
2021-09-18 00:10:39 -0700

    working on add new role

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ initilize();
 
 function initilize() {
   prompt
-  ([
+    ([
       {
         type: 'list',
         message: 'What would you like to do?',
@@ -29,11 +29,13 @@ function initilize() {
           db.query(`SELECT id AS 'DEPARTMENT ID', name AS 'DEPARTMENT NAME' FROM department`, function (err, results) {
             console.table(results);
           });
+          initilize();
           break;
         case 'view all roles':
           db.query(`SELECT role.title AS 'JOB TITLE', role.id AS 'ROLE ID', department.name AS 'DEPARTMENT', role.salary as 'SALARY' FROM department JOIN role ON role.department_id = department.id`, function (err, results) {
             console.table(results);
           });
+          initilize();
           break;
         case 'view all employees':
           db.query(`SELECT employee.id AS 'EMPLOYEE ID' , employee.first_name AS 'FIRST', employee.last_name AS 'LAST', role.title AS 'JOB TITLE', department.name as 'DEPARTMENT', role.salary as 'SALARY', employee.manager_id as 'MANAGER' 
@@ -42,9 +44,12 @@ function initilize() {
         JOIN employee ON employee.role_id = role.id`, function (err, results) {
             console.table(results);
           });
+          initilize();
           break;
         case 'add a department':
           addDepartment();
+        case 'add a role':
+          addRole();
         default:
           break;
       }
@@ -65,7 +70,35 @@ function addDepartment() {
     .then((res) => {
       const name = res;
       db.query(`INSERT INTO department (name) VALUES ('${name.name}')`)
-        console.log(`Added ${name.name} as a new department`)
-        initilize();
+      console.log(`Added ${name.name} as a new department`)
+      initilize();
+    })
+}
+
+function addRole() {
+  prompt([
+    {
+      type: 'input',
+      message: 'Title?',
+      name: 'title'
+    },
+    {
+      type: 'input',
+      message: 'Salary?',
+      name: 'salary'
+    },
+    {
+      type: 'input',
+      message: 'Department?',
+      // this can't just be an input, needs to be a list from departments
+      name: 'department_name',
+    },
+  ])
+    .then((res) => {
+      const newRole = res;
+      // need to enter a department_id to match with department?
+      db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${newRole.title}', ${newRole.salary}, ${newRole.department_name})`)
+      console.log(`Added ${newRole.title}`)
+      initilize();
     })
 }
