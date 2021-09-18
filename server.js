36c605b68ed6b680ffa48336cd9985b5cc386914
Chris Tolnai <christolnai@gmail.com>
2021-09-17 19:12:04 -0700

    added config file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: '',
+    database: 'movies_db'
+  },
+  console.log(`Connected to the movies_db database.`)
+);
