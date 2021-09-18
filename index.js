3e43e874b5e5f4bedbe94ea205499fcb0fbb6f1b
Chris Tolnai <christolnai@gmail.com>
2021-09-17 19:57:30 -0700

    fixed server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const express = require('express');
 const sequelize = require('./config/connection');
+const inquirer = require('inquirer');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
