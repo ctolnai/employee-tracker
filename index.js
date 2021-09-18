8d54d7f3330bc3eea300a395d12c81d5ffc3394e
Chris Tolnai <christolnai@gmail.com>
2021-09-17 22:05:18 -0700

    added results for 2nd and 3rd inquirer question

--- index.js
+++ index.js
@@ -15,20 +15,38 @@ app.use(express.urlencoded({ extended: false }));
 
 inquirer
   .prompt([
-  {
-    type: 'list',
-        message: 'What would you like to do?',
-        name: 'todo',
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role' ]
-  }
-])
-.then((res => {
-  const todo = res.todo;
-  console.log (res.todo)
-  db.query('SELECT * FROM department', function (err, results) {
-    console.table(results);
-  });
-}));
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'todo',
+      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+    }
+  ])
+  .then((res => {
+    const todo = res.todo;
+    console.log(res.todo)
+    switch (res.todo) {
+      case 'view all departments':
+        db.query(`SELECT name AS 'DEPARTMENT' FROM department`, function (err, results) {
+          console.table(results);
+        });
+        break;
+      case 'view all roles':
+        db.query(`SELECT title AS 'ROLES' FROM role`, function (err, results) {
+          console.table(results);
+        });
+        break;
+      case 'view all employees':
+        db.query(`SELECT CONCAT(first_name, ' ', last_name) AS 'EMPLOYEES'
+        FROM employee`, function (err, results) {
+          console.table(results);
+        });
+        break;
+      default:
+        break;
+    }
+
+  }));
 
 
 
